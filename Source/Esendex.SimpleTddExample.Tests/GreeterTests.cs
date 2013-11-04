be86e80dcff615f58f0f12526b855653daf0ab7e
Neil Kilbride <neil@neilkilbride.com>
2013-11-04 22:13:06 +0000

    Reverted example back to start

--- Source/Esendex.SimpleTddExample.Tests/GreeterTests.cs
+++ Source/Esendex.SimpleTddExample.Tests/GreeterTests.cs
@@ -9,23 +9,9 @@ namespace Esendex.SimpleTddExample.Tests
     public class GreeterTests
     {
         [Test]
-        public void GenerateGreeting_WhenNameAnn_ThenNiceToMeetYouAnn()
-        {
-            Assert.That(Greeter.GenerateGreeting("Ann"), Is.EqualTo("Nice to meet you Ann"));
-        }
-
-        [Test]
         public void GenerateGreeting_WhenNameSam_ThenSamIsAwesome()
         {
             Assert.That(Greeter.GenerateGreeting("Sam"), Is.EqualTo("Sam, you are awesome!"));
         }
-
-        [Test]
-        public void GenerateGreeting_WhenNameNeil_ThenNeilIsAwesome()
-        {
-            Assert.That(Greeter.GenerateGreeting("Neil"), Is.EqualTo("Neil, you are awesome!"));
-
-        }
-
     }
 }
--- Source/Esendex.SimpleTddExample/Greeter.cs
+++ Source/Esendex.SimpleTddExample/Greeter.cs
@@ -6,7 +6,7 @@ namespace Esendex.SimpleTddExample
     {
         public static string GenerateGreeting(string name)
         {
-            if (name == "Sam" || name == "Neil")
+            if (name == "Sam")
                 return name + ", you are awesome!";
 
             return string.Format("Nice to meet you {0}", name);
